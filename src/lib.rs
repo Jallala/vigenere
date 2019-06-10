9e2b39dc1d8fa1fdd8ca53a5bf4696b4253975e7
Jonatan Ojala <jojala@igpte.ch>
2019-06-10 12:51:35 +0200

    * Remove panic implementations  This is because they are not needed when build isn't `bin` or `dylib`.

--- src/lib.rs
+++ src/lib.rs
@@ -3,32 +3,11 @@
 #[macro_use]
 extern crate alloc;
 
-#[cfg(not(any(feature = "test", test)))]
-use core::intrinsics::abort;
-#[cfg(not(any(feature = "test", test)))]
-use core::panic::PanicInfo;
-
-
 #[cfg(any(feature = "test", test))]
 use wee_alloc;
 #[cfg(any(feature = "test", test))]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[cfg(not(any(feature = "test", test)))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    unsafe { abort() }
-}
-
-#[cfg(not(any(feature = "test", test)))]
-#[alloc_error_handler]
-fn foo(_: core::alloc::Layout) -> ! {
-    unsafe { abort() }
-}
-#[cfg(not(any(feature = "test", test)))]
-#[lang = "eh_personality"]
-extern fn eh_personality() {}
 
 pub mod key;
 pub mod table;
